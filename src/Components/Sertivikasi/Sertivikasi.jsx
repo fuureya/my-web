37ec4277f033cb01cbef3cce35f183b583b871ef
fuureya <agiljibrin009@gmail.com>
2024-06-02 00:15:34 +0800

    feat: kalimat sertivikasi

--- src/Components/Sertivikasi/Sertivikasi.jsx
+++ src/Components/Sertivikasi/Sertivikasi.jsx
@@ -4,7 +4,16 @@ const Sertivikasi = () => {
   return (
     <div>
       <Layout>
-        <h3 className="text-white">a</h3>
+        <h3 className="text-white text-lg text-center font-bold mb-3 pt-10">
+          Sertivikasi
+        </h3>
+        <p className="text-gray-300 md:text-lg text-sm text-justify">
+          Saya memiliki sertifikasi kompetensi yang menegaskan kemampuan dan
+          pengetahuan saya dalam bidang tertentu. Sertifikasi ini merupakan
+          bukti tanggung jawab saya terhadap pengembangan diri dan kualitas
+          kerja, serta menunjukkan dedikasi saya untuk meningkatkan keterampilan
+          dalam karier saya.
+        </p>
       </Layout>
     </div>
   );
